779947981700e9f1954ea577ba096569350bc0aa
Satyam Shubham <satyamsworld@gmail.com>
2021-09-01 23:59:58 +0530

    s

--- September/Day1/code.java
+++ September/Day1/code.java
@@ -190,3 +190,35 @@ import java.util.*;
             return prod_arr;
         } 
     }
+
+    // Last question ka dusra method dekhna hai 
+    // Last question ka dusra method dekhna hai 
+    // Last question ka dusra method dekhna hai 
+    // Last question ka dusra method dekhna hai 
+
+    class Solution
+{
+    static int searchInsertK(int Arr[], int N, int k)
+    {
+        int s = 0;
+        int e = N-1;
+        int ans = N;
+        
+        while(s<=e)
+        {
+            int mid = s + (e-s)/2;
+                    
+            if(Arr[mid] < k)
+            {
+                s = mid+1;
+            }
+            else 
+            {
+                ans = mid;
+                e = mid-1;
+            }
+        }
+        return ans;
+                
+    }
+}
