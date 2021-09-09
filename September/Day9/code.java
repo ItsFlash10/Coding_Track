a0bd647d6f63948d0c87aa6179d908f4c48f3e9c
Satyam Shubham <satyamsworld@gmail.com>
2021-09-09 22:47:12 +0530

    Frequencies of Limited Range Array Elements

--- September/Day9/code.java
+++ September/Day9/code.java
@@ -1,4 +1,5 @@
 package Day9;
+import java.util.*;
 
 // 1. Find Missing And Repeating
 //https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1#
@@ -26,5 +27,27 @@ class missingRepeating{
             }
         }
         return res;
+    }
+}
+
+//2. Frequencies of Limited Range Array Elements
+//https://practice.geeksforgeeks.org/problems/frequency-of-array-elements-1587115620/1/?track=md-arrays&batchId=144#
+
+class Solution{
+    //Function to count the frequency of all elements from 1 to N in the array.
+    public static void frequencyCount(int arr[], int N, int P)
+    {
+        int maxi = Math.max(P,N);
+        int[] map = new int[maxi+1];
+        
+        for(int i=0; i<N; i++)
+        {
+            map[arr[i]]++;
+        }
+        
+        for(int i=0; i<N; i++)
+        {
+            arr[i] = map[i+1];
+        }
     }
 }
