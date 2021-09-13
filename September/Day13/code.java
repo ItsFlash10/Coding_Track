484cb1d7671eb66c8db530508a1da020a1d605c7
Satyam Shubham <satyamsworld@gmail.com>
2021-09-13 23:10:36 +0530

    Two Sum (GFG)

--- September/Day13/code.java
+++ September/Day13/code.java
@@ -121,3 +121,27 @@ class cmnEle
         return intersection;
     }
 }
+
+// Two Sum(GFG)
+//https://practice.geeksforgeeks.org/problems/key-pair1554/0/?track=md-hashing&batchId=144#
+
+class twoSum
+{
+    // A[] : the input array of positive integers
+    // N : size of the array arr[]
+    // X : the required sum
+    public boolean keypair(int[] A, int N, int X)
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i<N; i++)
+        {
+            if(map.containsKey(X-A[i]))
+            {
+                return true;
+            }
+            map.put(A[i], i);
+        }
+        return false;
+    }
+}
