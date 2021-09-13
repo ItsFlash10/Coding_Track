0cb3b6fc0e2b17017247961c1cb3e217788d89e7
Satyam Shubham <satyamsworld@gmail.com>
2021-09-14 01:13:06 +0530

    3. Longest Substring Without Repeating Characters

--- September/Day14/code.java
+++ September/Day14/code.java
@@ -47,3 +47,51 @@ class pairGivenSum{
     }
 }
 
+// 3. Longest Substring Without Repeating Characters
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class longestSubString{
+    public int lengthOfLongestSubstring(String s) {
+        
+        //O(N) using MAP -> BEST
+        HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+        int res = 0;
+        int left = 0;
+        int right = 0;
+        
+        while(right<s.length())
+        {
+            if(map.containsKey(s.charAt(right)))
+            {
+                left = Math.max(map.get(s.charAt(right))+1, left);
+            }
+            map.put(s.charAt(right), right);
+            
+            res = Math.max(res, right-left+1);
+            right++;
+        }
+        return res;
+
+        // //O(2N) using SET
+        // HashSet<Character> set = new HashSet<>();
+        // int res = 0;
+        // int left = 0;
+        // int right = 0;
+        
+        // while(left < s.length() && right < s.length())
+        // {
+        //     if(!set.contains(s.charAt(right)))
+        //     {
+        //         set.add(s.charAt(right));
+        //         right++;
+        //         res = Math.max(res, set.size());
+        //     }
+        //     else
+        //     {
+        //         set.remove(s.charAt(left));
+        //         left++;
+        //     }
+        // }
+        // return res;        
+    }
+}
