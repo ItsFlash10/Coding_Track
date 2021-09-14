96e81463fe67e236deedd815e820495729e1b99a
Satyam Shubham <satyamsworld@gmail.com>
2021-09-14 16:24:11 +0530

    56. Merge Intervals

--- September/Day14/code.java
+++ September/Day14/code.java
@@ -95,3 +95,42 @@ class longestSubString{
         // return res;        
     }
 }
+
+//56. Merge Intervals
+//https://leetcode.com/problems/merge-intervals/
+
+class mergeIntervals{
+    public int[][] merge(int[][] intervals) {
+        
+        List<int[]> res = new ArrayList<>();
+        
+        if(intervals.length == 0 || intervals == null)
+        {
+            return res.toArray(new int[0][]);
+        }
+        
+        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
+        
+        for(int[] interval : intervals)
+        {
+            if(res.size() == 0)
+            {
+                res.add(interval);
+            }
+            else
+            {
+                int[] prevInterval = res.get(res.size()-1);
+                
+                if(interval[0] <= prevInterval[1])
+                {
+                    prevInterval[1] = Math.max(prevInterval[1], interval[1]);
+                }
+                else
+                {
+                    res.add(interval);
+                }
+            }
+        }
+        return res.toArray(new int[0][]);      
+    }
+}
