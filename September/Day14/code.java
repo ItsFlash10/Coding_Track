137445a0a8655e08655a6157428b207fae6b95c2
Satyam Shubham <satyamsworld@gmail.com>
2021-09-14 18:08:03 +0530

    217. Contains Duplicate

--- September/Day14/code.java
+++ September/Day14/code.java
@@ -134,3 +134,27 @@ class mergeIntervals{
         return res.toArray(new int[0][]);      
     }
 }
+
+//217. Contains Duplicate
+//https://leetcode.com/problems/contains-duplicate/
+
+class duplicateCheck{
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(!set.contains(nums[i]))
+            {
+                set.add(nums[i]);
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
