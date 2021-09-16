18351fd37890a5057d31acc85481608d95070aca
Satyam Shubham <satyamsworld@gmail.com>
2021-09-16 17:12:44 +0530

    First negative integer in every window of size k

--- September/Day16/code.java
+++ September/Day16/code.java
@@ -31,3 +31,44 @@ class maxSubArrayInK{
        return max;
     }
 }
+
+//First negative integer in every window of size k 
+//https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1#
+
+class firstNegativeInWindow{
+    
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    {
+        int i = 0;
+        int j = 0;
+        long[] res = new long[N-K+1];
+        ArrayList<Long> range = new ArrayList<>();
+        
+        while(j < N)
+        {
+            if(A[j] < 0)
+            {
+                range.add(A[j]);
+            }
+
+            if(j-i+1 == K)
+            {
+                if(range.size() == 0)
+                {
+                    res[i] = 0;
+                }
+                else if(A[i] == range.get(0))
+                {
+                    res[i] = range.remove(0);
+                }
+                else
+                {
+                    res[i] = range.get(0);
+                }
+                i++;
+            }
+            j++;
+        }
+        return res;
+    }
+}
