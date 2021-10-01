dc2793e7386bf4ce4ca289dcaa9c5a30d6274eef
Satyam Shubham <satyamsworld@gmail.com>
2021-10-01 15:56:24 +0530

    Created function to add parameter for lengths in the function

--- October/Day1/code.java
+++ October/Day1/code.java
@@ -3,25 +3,26 @@ package Day1;
 //Longest Common Subsequence Recursive method
 
 class LCS{
-    public int longestCommonSubsequence(String text1, String text2) {
-        
-        int m = text1.length();
-        int n = text2.length();
-        
+    
+    static int stringLength(String text1 , String text2, int m, int n)
+    {
         if(m == 0 || n == 0) return 0;
         
         if(text1.charAt(m-1) == text2.charAt(n-1))
         {
-            String t1 = text1.substring(0, m-1);
-            String t2 = text2.substring(0, n-1);
-            return 1 + longestCommonSubsequence(t1, t2);
+            return 1 + stringLength(text1, text2, m-1, n-1);
         }
         else
         {
-            String t1 = text1.substring(0, m-1);
-            String t2 = text2.substring(0, n-1);
-            
-            return Math.max(longestCommonSubsequence(t1, text2), longestCommonSubsequence(text1, t2));
+            return Math.max(stringLength(text1, text2, m-1, n), stringLength(text1, text2, m, n-1));
         }
+
+    }
+    static int longestCommonSubsequence(String text1, String text2) {
+        
+        int m = text1.length();
+        int n = text2.length();
+
+       return  stringLength(text1, text2, m, n);       
     }
 }
