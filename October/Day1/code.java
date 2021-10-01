f6aff273d2d80c33dbd554b4550dff537c143ccf
Satyam Shubham <satyamsworld@gmail.com>
2021-10-01 23:59:21 +0530

    Check if Linked List is Palindrome

--- October/Day1/code.java
+++ October/Day1/code.java
@@ -90,3 +90,36 @@ class Swap
         // return head;
     }   
 }
+
+//Check if Linked List is Palindrome
+//https://practice.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1/?track=md-linkedlist&batchId=144
+
+class checkPalin
+{
+    //Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head) 
+    {
+        Node slow = head, fast = head, prev, temp;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+        while (slow != null) {
+            temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        fast = head;
+        slow = prev;
+        while (slow != null) {
+            if (fast.data != slow.data) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }    
+}
