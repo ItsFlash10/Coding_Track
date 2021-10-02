887a3d1884518149b5a8aa8f10c302bf3d80c94d
Satyam Shubham <satyamsworld@gmail.com>
2021-10-02 10:19:50 +0530

    121. Best Time to Buy and Sell Stock

--- October/Day2/code.java
+++ October/Day2/code.java
@@ -98,3 +98,21 @@ class intrsecEle {
         return res;
     }
 }
+
+//121. Best Time to Buy and Sell Stock
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class maximProfit {
+    public int maxProfit(int[] prices) {
+        
+        int profit = 0;
+        int minPrice = prices[0];
+        
+        for(int i = 0; i<prices.length; i++)
+        {
+            minPrice = Math.min(minPrice, prices[i]);
+            profit = Math.max(profit, prices[i] - minPrice);
+        }
+        return profit;
+    }
+}
