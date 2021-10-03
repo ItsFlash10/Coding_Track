ec741f14cb5051d0b8bfb40194b2fa3ef8ac46e9
Satyam Shubham <satyamsworld@gmail.com>
2021-10-03 17:06:04 +0530

    118. Pascal's Triangle

--- October/Day3/code.java
+++ October/Day3/code.java
@@ -51,3 +51,31 @@ class reshapeMatrix{
         return res; 
     }
 }
+
+//118. Pascal's Triangle
+//https://leetcode.com/problems/pascals-triangle/submissions/
+
+class pascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        
+        for(int i = 0; i<numRows; i++)
+        {
+            ArrayList<Integer> ans = new ArrayList<>();
+            for(int j = 0; j<=i; j++)
+            { 
+                if(j == 0 || j == i)
+                {
+                    ans.add(1);
+                }
+                else
+                {
+                    ans.add(res.get(i-1).get(j-1) + res.get(i-1).get(j));
+                }
+            }
+            res.add(ans);
+        }
+        return res;
+    }
+}
