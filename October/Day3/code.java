9ba13eac5c3551ed836c0faca07b60be1ae4b485
Satyam Shubham <satyamsworld@gmail.com>
2021-10-04 00:22:57 +0530

    142. Linked List Cycle II

--- October/Day3/code.java
+++ October/Day3/code.java
@@ -181,3 +181,44 @@ class rmv
         }
     }
 }
+
+//142. Linked List Cycle II
+//https://leetcode.com/problems/linked-list-cycle-ii/
+
+
+ //Definition for singly-linked list.
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
+ 
+class cycleNode{
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head == null || head.next == null) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode entry = head;
+        
+        while(fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                while(slow != entry)
+                {
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
