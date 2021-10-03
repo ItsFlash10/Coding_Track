f351d85bfbfa0af2292760b6f339b11ac0b13eb7
Satyam Shubham <satyamsworld@gmail.com>
2021-10-03 15:52:28 +0530

    566. Reshape the Matrix

--- October/Day3/code.java
+++ October/Day3/code.java
@@ -19,3 +19,35 @@ class jumpGame{
         return true;
     }
 }
+
+//566. Reshape the Matrix
+//https://leetcode.com/problems/reshape-the-matrix/
+
+class reshapeMatrix{
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        
+        int row = mat.length;
+        int col = mat[0].length;
+        
+        if((row*col) != (r*c)) return mat;
+ 
+        int res[][] = new int[r][c];
+        int rowNum = 0;
+        int colNum = 0;
+     
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                res[rowNum][colNum] = mat[i][j];
+                colNum++;
+                if(colNum == c)
+                {
+                    colNum = 0;
+                    rowNum++;
+                }
+            }
+        }
+        return res; 
+    }
+}
