0126d45074cbdfc6e3c12cd72defde5d0c1a6593
Satyam Shubham <satyamsworld@gmail.com>
2021-10-05 00:31:10 +0530

    148. Sort List

--- October/Day4/code.java
+++ October/Day4/code.java
@@ -88,3 +88,65 @@ class calcPerim{
         return perimeter;
     }
 }
+
+//148. Sort List
+//https://leetcode.com/problems/sort-list/
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        
+        if(head == null || head.next == null) return head;
+        
+        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null)
+        {
+            temp = slow;     
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        temp.next = null;
+        
+        ListNode lsh = sortList(head);
+        ListNode rsh = sortList(slow);
+        
+        return merge(lsh, rsh);
+    }
+    
+    static ListNode merge(ListNode l1, ListNode l2)
+    {
+        ListNode sortedTmp = new ListNode(0);
+        ListNode curr = sortedTmp;
+        
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val < l2.val)
+            {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        
+        if(l1 != null)
+        {
+            curr.next = l1;
+            l1 = l1.next;
+        }
+        
+        if(l2 != null)
+        {
+            curr.next = l2;
+            l2 = l2.next;
+        }
+        
+        return sortedTmp.next;
+    }
+}
