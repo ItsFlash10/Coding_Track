5e89b35aa78e2344920e02b351197ddcfe45cb9e
Satyam Shubham <satyamsworld@gmail.com>
2021-10-05 21:01:40 +0530

    70. Climbing Stairs

--- October/Day5/code.java
+++ October/Day5/code.java
@@ -93,3 +93,23 @@ class ranNote{
         return count == 0 ? true : false;  
     }
 }
+
+//70. Climbing Stairs [Daily Challenge][DP]
+//https://leetcode.com/problems/climbing-stairs/
+
+class waysToClimb {
+    public int climbStairs(int n) {
+        
+        int[] dp = new int[n+1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+}
