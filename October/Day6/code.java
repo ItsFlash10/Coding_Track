1f0599f41a6cc4dc443a0fd5949bc6e317470e89
Satyam Shubham <satyamsworld@gmail.com>
2021-10-06 18:00:44 +0530

    141. Linked List Cycle [DC]

--- October/Day6/code.java
+++ October/Day6/code.java
@@ -241,3 +241,33 @@ class rotationArr{
         }
     }
 }
+
+//141. Linked List Cycle [DC]
+//https://leetcode.com/problems/linked-list-cycle/submissions/
+
+//Definition for singly-linked list.
+ class ListNode {
+   int val;
+   ListNode next;
+   ListNode(int x){
+       val = x;
+       next = null;
+    }
+ }
+
+class checkCycle{
+    public boolean hasCycle(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}
