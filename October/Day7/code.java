e5f6b93f407a3fd3431cc9a2dcc25118c5878b18
Satyam Shubham <satyamsworld@gmail.com>
2021-10-07 23:50:27 +0530

    83. Remove Duplicates from Sorted List

--- October/Day7/code.java
+++ October/Day7/code.java
@@ -152,3 +152,26 @@ class rotateLL{
         return head;
     }
 }
+
+//83. Remove Duplicates from Sorted List
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+class delDup{
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode curr = head;
+        
+        while(curr != null && curr.next != null)
+        {
+            if(curr.val == curr.next.val)
+            {
+                curr.next = curr.next.next;                                
+            }
+            else
+            {
+                curr = curr.next;
+            }
+        }
+        return head;        
+    }
+}
