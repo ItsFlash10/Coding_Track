7ea9fb249b080542b422b6ba02c25a6330555887
Satyam Shubham <satyamsworld@gmail.com>
2021-10-12 03:02:02 +0530

    567. Permutation in String

--- October/Day12/code.java
+++ October/Day12/code.java
@@ -37,3 +37,55 @@ class revWrdsInString{
         }
     }
 }
+
+//567. Permutation in String
+//https://leetcode.com/problems/permutation-in-string/
+
+class stringPermutation{
+    public boolean checkInclusion(String s1, String s2) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < s1.length(); i++)
+        {
+            map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0)+1);
+        }
+        
+        int i = 0;
+        int j = 0;
+        int k = s1.length();
+        int count = map.size();
+        
+        while(j < s2.length())
+        {
+            char ch = s2.charAt(j);
+            
+            if(map.containsKey(ch))
+            {
+                map.put(ch,map.getOrDefault(ch,0)-1);
+                
+                if(map.get(ch) == 0)
+                {
+                    count--;
+                }
+            }
+            if(j-i+1 == k)
+            {
+                if(count == 0)
+                {
+                    return true;
+                }
+                
+                if(map.containsKey(s2.charAt(i)))
+                {
+                    map.put(s2.charAt(i) , map.get(s2.charAt(i))+1);
+                    
+                    if(map.get(s2.charAt(i)) == 1) count ++;
+                }
+                i++;
+            }
+            j++;
+        }
+        return false;  
+    }
+}
