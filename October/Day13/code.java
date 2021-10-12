7ab988cf57e2df30675a258a842188f41766c31e
Satyam Shubham <satyamsworld@gmail.com>
2021-10-13 03:26:15 +0530

    54. Spiral Matrix

--- October/Day13/code.java
+++ October/Day13/code.java
@@ -1,4 +1,5 @@
 package Day13;
+import java.util.*;
 
 //Flattening a Linked List
 //https://practice.geeksforgeeks.org/problems/flattening-a-linked-list/0/?track=md-linkedlist&batchId=144
@@ -57,5 +58,63 @@ class GfG
         if(l1 != null) temp.bottom = l1;
         else temp.bottom = l2;
         return res.bottom;
+    }
+}
+
+//54. Spiral Matrix
+//https://leetcode.com/problems/spiral-matrix/
+
+class spiralPrint{
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        int top = 0;
+        int down = n-1;
+        int left = 0;
+        int right = m-1;
+        
+        int dir = 0;  //this wil give the direction of progress
+        
+        while(right >= left  && top <= down)
+        {
+            if(dir == 0)  //left to right
+            {
+                for(int i = left; i<=right; i++)
+                {
+                    res.add(matrix[top][i]);                    
+                }
+                top++;
+            }
+            else if(dir == 1) //top to bottom 
+            {
+                for(int i = top; i<=down; i++)
+                {
+                    res.add(matrix[i][right]);                    
+                }
+                right--;
+            }
+            else if(dir == 2) //right to left
+            {
+                for(int i = right; i>=left; i--)
+                {
+                    res.add(matrix[down][i]);                    
+                }
+                down--;
+            }
+            else if(dir == 3) //bottom to up
+            {
+                for(int i = down; i>=top; i--)
+                {
+                    res.add(matrix[i][left]);                    
+                }
+                left++;
+            }
+            dir = (dir+1)%4; //point to remember for changing direction
+        }
+        return res;  
     }
 }
