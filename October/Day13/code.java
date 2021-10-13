be5f79c2fd0dac8f4ae6ba2ac848752951dc9bfe
Satyam Shubham <satyamsworld@gmail.com>
2021-10-13 17:32:05 +0530

    25. Reverse Nodes in k-Group

--- October/Day13/code.java
+++ October/Day13/code.java
@@ -118,3 +118,55 @@ class spiralPrint{
         return res;  
     }
 }
+
+//25. Reverse Nodes in k-Group
+//https://leetcode.com/problems/reverse-nodes-in-k-group/submissions/
+
+//Definition for singly-linked list.
+ class ListNode
+ {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+class revKGrp{
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+        if(head == null || k == 1) return head;
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode prev = dummy;
+        ListNode curr = dummy;
+        ListNode nex = dummy;
+        
+        int count = 0;
+        
+        while(curr.next != null)
+        {
+            curr = curr.next;
+            count++;
+        }
+        
+        while(count >= k)
+        {
+            curr = prev.next;
+            nex = curr.next;
+                
+            for(int i = 1; i<k; i++)
+            {
+                curr.next = nex.next;
+                nex.next = prev.next;
+                prev.next = nex;
+                nex = curr.next;
+            }
+            count -= k;
+            prev = curr;
+        }
+        return dummy.next;
+    }
+}
