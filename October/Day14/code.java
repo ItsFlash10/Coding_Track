32dea86007114ffdbccb4b7d833d943b9337a3df
Satyam Shubham <satyamsworld@gmail.com>
2021-10-14 06:12:30 +0530

    279. Perfect Squares

---
+++ October/Day14/code.java
@@ -0,0 +1,26 @@
+package Day14;
+
+//279. Perfect Squares
+//https://leetcode.com/problems/perfect-squares/
+
+class perfectSqr{
+    public int numSquares(int n) {
+        
+        int[] dp = new int[n+1];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i<=n; i++)
+        {
+            int min = Integer.MAX_VALUE;
+            for(int j = 1; j*j <= i; j++)
+            {
+                int rem = i - j*j;
+                min = Math.min(dp[rem], min);
+            }
+            dp[i] = min+1;
+        }
+        return dp[n];        
+    }
+}
