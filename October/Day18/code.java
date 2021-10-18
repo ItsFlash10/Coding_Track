de8a061396e9ab28c22c6b0785fa7d218eae598c
Satyam Shubham <satyamsworld@gmail.com>
2021-10-18 23:50:37 +0530

    617. Merge Two Binary Trees

--- October/Day18/code.java
+++ October/Day18/code.java
@@ -169,3 +169,22 @@ class areaOfIsland {
         return count;
     }
 }
+
+//617. Merge Two Binary Trees
+//https://leetcode.com/problems/merge-two-binary-trees/submissions/
+
+class mergingTrees{
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        
+        if(root1 == null) return root2;
+        
+        if(root2 == null) return root1;
+        
+        root1.val += root2.val;
+        
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        
+        return root1;        
+    }
+}
