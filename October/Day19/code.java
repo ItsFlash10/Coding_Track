31dc9f2e65c39c458682b41e45f047ab84e22501
Satyam Shubham <satyamsworld@gmail.com>
2021-10-19 01:00:43 +0530

    116. Populating Next Right Pointers in Each Node

--- October/Day19/code.java
+++ October/Day19/code.java
@@ -50,3 +50,53 @@ class XORforDuplicate{
         return res;        
     }
 }
+
+//116. Populating Next Right Pointers in Each Node
+//https://leetcode.com/problems/populating-next-right-pointers-in-each-node/submissions/
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+
+class popRight{
+    public Node connect(Node root) {
+        
+        Node black = root;
+        
+        while(black != null && black.left != null)
+        {
+            Node dummy = black;
+            
+            while(true)
+            {
+                dummy.left.next = dummy.right;
+                
+                if(dummy.next == null) break;
+                
+                dummy.right.next = dummy.next.left;
+                
+                dummy = dummy.next;
+            }
+            black = black.left;
+        }
+        return root;
+    }
+}
