8909efc830603bd12fba0a4122387c3702a15f4e
Satyam Shubham <satyamsworld@gmail.com>
2021-10-19 01:39:31 +0530

    542. 01 Matrix

--- October/Day19/code.java
+++ October/Day19/code.java
@@ -1,4 +1,5 @@
 package Day19;
+import java.util.*;
 
 //191. Number of 1 Bits
 //https://leetcode.com/problems/number-of-1-bits/
@@ -98,5 +99,59 @@ class popRight{
             black = black.left;
         }
         return root;
+    }
+}
+
+//542. 01 Matrix
+//https://leetcode.com/problems/01-matrix/
+
+class sol{
+    public int[][] updateMatrix(int[][] mat) {
+        
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for(int i = 0; i<mat.length; i++)
+        {
+            for(int j = 0; j<mat[0].length; j++)
+            {
+                if(mat[i][j] == 0)
+                {
+                    queue.add(new int[]{i,j});
+                }
+                    
+                else
+                {
+                    mat[i][j] = -1;
+                }
+            }
+        }
+        
+        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+        
+        int dis = 0;
+        
+        while(!queue.isEmpty())
+        {
+            int size = queue.size();
+            dis++;
+            
+            while(size-- > 0)
+            {
+                int[] cell = queue.poll();
+                
+                for(int[] dir : dirs)
+                {
+                    int r = cell[0] + dir[0];
+                    int c = cell[1] + dir[1];
+                    
+                    if(r<0 || c<0 || r==mat.length || c ==mat[0].length || mat[r][c] != -1)
+                        continue;
+                    
+                    mat[r][c] = dis;
+                    queue.add(new int[] {r,c});
+                }
+            }
+        }
+        return mat;        
     }
 }
