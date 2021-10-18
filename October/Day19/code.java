a573fdc2728da6c31a6f19cef3e5f980d01126b4
Satyam Shubham <satyamsworld@gmail.com>
2021-10-19 00:40:23 +0530

    190. Reverse Bits

--- October/Day19/code.java
+++ October/Day19/code.java
@@ -16,3 +16,20 @@ class answer{
         return ones;
     }
 }
+
+//190. Reverse Bits
+//https://leetcode.com/problems/reverse-bits/submissions/
+
+class revBits{
+    public int reverseBits(int n) {
+        
+        int rev=0;
+        
+        for(int i=0;i<32;i++)
+        {
+            rev= (rev << 1) | (n & 1);         
+            n = n >> 1;                  
+        }
+        return rev;
+    }
+}
