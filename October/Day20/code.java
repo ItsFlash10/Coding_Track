8350ddb428a93710f12c35321661d3cbfcd9d212
Satyam Shubham <satyamsworld@gmail.com>
2021-10-20 16:08:17 +0530

    151. Reverse Words in a String

--- October/Day20/code.java
+++ October/Day20/code.java
@@ -16,3 +16,51 @@ class missingNum {
         return res;        
     }
 }
+
+//151. Reverse Words in a String
+//https://leetcode.com/problems/reverse-words-in-a-string/
+
+class revWords{
+    public String reverseWords(String s) {
+        
+        int i = s.length()-1;
+        String ans = "";
+        
+        while(i >= 0)
+        {
+            while(i >= 0 && s.charAt(i) == ' ') i--;
+            
+            int j = i;
+            
+            if(i < 0) break;
+            
+            while(i >= 0 && s.charAt(i) != ' ') i--;
+            
+            if(!ans.isEmpty())
+            {
+                // since j is excluded in substring fxn 
+               ans = ans.concat(" " + s.substring(i+1, j+1)); 
+            }
+            else
+            {
+               ans = ans.concat(s.substring(i+1, j+1));                
+            }
+        }
+        return ans;
+        
+           //Using StringBuilder
+//         String[] str = s.split(" ");
+//         StringBuilder sb = new StringBuilder();
+//         int end = str.length - 1;
+        
+//         for(int i = 0; i<= end; i++)
+//         {
+//             if(!str[i].isEmpty()) 
+//             {
+//                 sb.insert(0, str[i]);
+//                 if(i < end) sb.insert(0, " ");
+//             }
+//         }        
+//         return sb.toString();
+    }
+}
