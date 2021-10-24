10c6823628f49762553ae60a76114774648a2d51
Satyam Shubham <satyamsworld@gmail.com>
2021-10-24 23:49:08 +0530

    66. Plus One

--- October/Day24/code.java
+++ October/Day24/code.java
@@ -100,3 +100,28 @@ class prefixFinding{
         return prefix;        
     }
 }
+
+//66. Plus One
+//https://leetcode.com/problems/plus-one/
+
+class onePlus {
+    public int[] plusOne(int[] digits) {
+        
+        int n = digits.length;
+        
+        for(int i = n-1; i>=0; i--)
+        {
+            if(digits[i] != 9)
+            {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+            digits[i] = 0; //if equal too 9
+        }
+        
+        int[] res = new int[n+1]; // when 99, 999, etc
+        res[0] = 1;
+        
+        return res;
+    }
+}
