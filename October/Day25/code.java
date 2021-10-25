0551fb983a1a810e5d5bc8e5351b02241eef59bd
Satyam Shubham <satyamsworld@gmail.com>
2021-10-25 20:03:53 +0530

    155. Min Stack

--- October/Day25/code.java
+++ October/Day25/code.java
@@ -1,4 +1,5 @@
 package Day25;
+import java.util.*;
 
 //28. Implement strStr()
 //https://leetcode.com/problems/implement-strstr/
@@ -66,3 +67,46 @@ class GfG
     }
 }
 
+//155. Min Stack
+//https://leetcode.com/problems/min-stack/
+
+class MinStack {
+    Stack<int[] > stack;
+
+    public MinStack() {
+        
+        stack = new Stack<>();        
+    }
+    
+    public void push(int val) {
+        if(stack.isEmpty())
+        {
+            stack.push(new int[] {val, val});
+            return;
+        }
+        int currentMin = stack.peek()[1];
+        
+        stack.push(new int[] {val, Math.min(val, currentMin)});
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
