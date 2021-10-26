76b9f0f6562ee5cca189ff12a1d68ee4a1f72cba
Satyam Shubham <satyamsworld@gmail.com>
2021-10-27 00:00:15 +0530

    226. Invert Binary Tree

--- October/Day26/code.java
+++ October/Day26/code.java
@@ -3,6 +3,20 @@ package Day26;
 //226. Invert Binary Tree
 //https://leetcode.com/problems/invert-binary-tree/
 
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return null;
