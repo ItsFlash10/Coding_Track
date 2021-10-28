2c74d41bb03d86f03faf158efc69624a083a3ba6
Satyam Shubham <satyamsworld@gmail.com>
2021-10-29 00:05:27 +0530

    378. Kth Smallest Element in a Sorted Matrix

--- October/Day28/code.java
+++ October/Day28/code.java
@@ -63,3 +63,27 @@ class usingPQ {
         return minHeap.peek();        
     }
 }
+
+//378. Kth Smallest Element in a Sorted Matrix
+//https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+class kthSmallestInAMatrix {
+    public int kthSmallest(int[][] matrix, int k) {
+        
+        // int matLen = matrix.length*matrix[0].length;
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> (b - a));
+        
+        for(int i = 0; i<matrix.length; i++)
+        {
+            for(int j = 0; j<matrix[0].length; j++)
+            {
+                maxHeap.add(matrix[i][j]);
+                
+                if(maxHeap.size() > k) maxHeap.poll();
+            }
+        }
+        // while(maxHeap.size() > matLen - k + 1) maxHeap.remove();
+        return maxHeap.peek();
+    }
+}
