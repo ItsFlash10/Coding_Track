1a7aa03fe0b352fb089a43f4705b31eb8cc5879d
Satyam Shubham <satyamsworld@gmail.com>
2021-11-05 23:31:07 +0530

    441. Arranging Coins

---
+++ November/Day5/code.java
@@ -0,0 +1,32 @@
+package Day5;
+
+//441. Arranging Coins
+//https://leetcode.com/problems/arranging-coins/
+
+class BSVariant {
+    
+    public int arrangeCoins(int n) {
+        
+        long low = 1, high = n;
+        
+        while(low <= high){
+            
+            long mid = low + (high-low)/2;
+            long sum  = mid*(mid+1)/2;
+            
+            if(sum == n)
+            {
+                return (int)mid;
+            } 
+            else if(sum < n)
+            {
+                low = mid+1;
+            } 
+            else 
+            {
+                high = mid-1;
+            }
+        }
+        return (int)high;
+    }
+}
