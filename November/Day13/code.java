4e2e009fae4cd5b9bdf86b47f4b8a30dad728c29
Satyam Shubham <57027429+ItsFlash10@users.noreply.github.com>
2021-11-13 23:59:56 +0530

    739. Daily Temperatures

--- November/Day13/code.java
+++ November/Day13/code.java
@@ -1,1 +1,44 @@
 package Day13;
+
+//739. Daily Temperatures
+//https://leetcode.com/problems/daily-temperatures/
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        
+        int n = temperatures.length;
+        Stack<Integer> s = new Stack<>();
+        int[] ans = new int[n];
+        
+        for(int i = n-1; i>=0; i--)
+        {
+            if(s.empty())
+            {
+                ans[i] = 0;
+            }
+            
+            else if(!s.empty() && s.peek() > i)
+            {
+                ans[i] = s.peek();
+            }
+            else if(!s.empty() && s.peek() <= i)
+            {
+                while(!s.empty() && s.peek() <= i)
+                {
+                    s.pop();
+                }
+                if(s.empty())
+                {
+                    ans[i] = 0;
+                }
+                else
+                {
+                    ans[i] = s.peek();
+                }
+            }
+            s.push(i);
+        }
+        
+        return ans;   
+    }
+}
