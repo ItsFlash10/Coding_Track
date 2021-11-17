b08ee2263bbed23664cec448729f61a8275974e7
Satyam Shubham <57027429+ItsFlash10@users.noreply.github.com>
2021-11-17 22:35:00 +0530

    62. Unique Paths

---
+++ November/Day17/code.java
@@ -0,0 +1,22 @@
+package Day17;
+
+//62. Unique Paths
+//https://leetcode.com/problems/unique-paths/
+
+public class unqPaths {
+    public int uniquePaths(int m, int n) 
+    {
+        int[][] grid = new int[m][n];
+        for(int i = 0; i<m; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                if(i==0||j==0)
+                    grid[i][j] = 1;
+                else
+                    grid[i][j] = grid[i][j-1] + grid[i-1][j];
+            }
+        }
+        return grid[m-1][n-1];
+    }
+}
