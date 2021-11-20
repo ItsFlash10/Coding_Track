e602120cc9c771e968a0e1ee15a34a42181f603b
Satyam Shubham <satyamsworld@gmail.com>
2021-11-21 00:28:15 +0530

    540. Single Element in a Sorted Array (BS X BM)

--- November/Day20/code.java
+++ November/Day20/code.java
@@ -16,3 +16,26 @@ class xorSol{
         return res;
     }
 }
+
+//BS X BitsMan.
+// Note: How can you implement the check operation. Remember the xor operation, and if you xor any number with 1, then it flips the LSB (Least significant bit)
+// Ex. 1) 100^1=101 [i.e 4^1=5, flips the first bit from right side]
+//     2) 101^1=100 [i.e 5^1=4, again flips the first bit from right side]
+// So, if I is even, to find the next element we do I^1
+// and if I is odd, to find the previous element we do I^1
+
+class bSxBM {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int h = nums.length - 1;
+        while (l < h) {
+            int m = (l + h) / 2;
+            if (nums[m] == nums[m ^ 1]) {
+                l = m + 1;
+            } else {
+                h = m;
+            }
+        }
+        return nums[h];
+    }
+}
