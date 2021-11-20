05c9e59f2e5ef11a512df67375df09c952a68b9d
Satyam Shubham <satyamsworld@gmail.com>
2021-11-21 00:29:45 +0530

    540. Single Element in a Sorted Array (Binary Search)[PP]

--- November/Day20/code.java
+++ November/Day20/code.java
@@ -39,3 +39,60 @@ class bSxBM {
         return nums[h];
     }
 }
+
+//Using Modified Binary Search
+
+class PPSolu{
+    public int singleNonDuplicate(int[] nums) {
+        
+        int n = nums.length;
+        if(nums.length == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        
+        int high = n-1;
+        int low = 0;
+        
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+            {
+                return nums[mid];
+            }
+            
+            else if(nums[mid] == nums[mid - 1])
+            {
+                //count to find the odd side
+                int leftCount = mid - low + 1;
+                
+                if(leftCount % 2 == 0)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 2;
+                }
+            }
+            
+            else if(nums[mid] == nums[mid + 1])
+            {
+                //count to find the odd side
+                int rightCount = high - mid + 1;
+                
+                if(rightCount % 2 == 0)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid - 2;
+                }
+            }
+        }
+        return 0;
+    }
+}
+
