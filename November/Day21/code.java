d6439dfd405b55e06cc19378e26624ae8c8fd867
Satyam Shubham <satyamsworld@gmail.com>
2021-11-22 02:06:53 +0530

    106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ November/Day21/code.java
@@ -0,0 +1,45 @@
+package Day21;
+
+//106. Construct Binary Tree from Inorder and Postorder Traversal
+//https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class treeSolut{
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return build(inorder,0,inorder.length-1,postorder,0,postorder.length-1);
+    }
+    
+    public TreeNode build(int[] inorder, int inS, int inE, int[] postorder, int posS, int posE){
+        if(inS>inE || posS>posE) return  null;
+        
+        TreeNode root = new TreeNode(postorder[posE]);
+        
+        int rootI=0;
+        for(int i=0;i<inorder.length;i++)
+        {
+            if(inorder[i]==root.val){
+                rootI = i;
+                break;
+            }
+        }
+        
+        root.left = build(inorder,inS,rootI-1,postorder,posS,posS+rootI-inS-1);
+        root.right = build(inorder,rootI+1,inE,postorder,posS+rootI-inS,posE-1);
+        
+        return root;
+    }
+}
