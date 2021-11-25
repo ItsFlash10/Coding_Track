7b75a719dd1ffbc4bab8a88ef6169ae57b557302
Satyam Shubham <satyamsworld@gmail.com>
2021-11-25 23:39:28 +0530

    53. Maximum Subarray

---
+++ November/Day25/code.java
@@ -0,0 +1,24 @@
+package Day25;
+
+//53. Maximum Subarray
+//https://leetcode.com/problems/maximum-subarray/
+
+class mxSubArr {
+    public int maxSubArray(int[] nums) {
+        
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        
+        for(int i = 0; i<nums.length; i++)
+        {
+            sum += nums[i];
+            maxSum = Math.max(maxSum, sum);
+            
+            if(sum<0)
+            {
+                sum =0;
+            }
+        }
+        return maxSum;
+    }
+}
