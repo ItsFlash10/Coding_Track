100f251f90edc8284bd8068653a4b1c357a9127a
Satyam Shubham <satyamsworld@gmail.com>
2021-12-01 22:54:22 +0530

    198. House Robber

---
+++ December/Day1/code.java
@@ -0,0 +1,23 @@
+package Day1;
+
+//198. House Robber
+//https://leetcode.com/problems/house-robber/
+
+class houseRobberPrblm {
+    public int rob(int[] nums) {
+        
+        if(nums.length == 0) return 0;
+        
+        int[] dp = new int[nums.length+1];
+        
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i = 1; i<nums.length; i++)
+        {
+            dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
+        }
+        
+        return dp[nums.length];
+    }
+}
