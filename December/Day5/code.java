2dba54a04f1a902268d384954a0f32319b13fedc
Satyam Shubham <satyamsworld@gmail.com>
2021-12-05 23:46:53 +0530

    337. House Robber III

---
+++ December/Day5/code.java
@@ -0,0 +1,35 @@
+package Day5;
+
+//337. House Robber III
+//https://leetcode.com/problems/house-robber-iii/
+
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class houserobberIII {
+    public int rob(TreeNode root) {
+        int[] num = dfs(root);
+        return Math.max(num[0], num[1]);
+    }
+    private int[] dfs(TreeNode x) {
+        if (x == null) return new int[2];
+        int[] left = dfs(x.left);
+        int[] right = dfs(x.right);
+        int[] res = new int[2];
+        res[0] = left[1] + right[1] + x.val;
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return res;
+    }
+}
