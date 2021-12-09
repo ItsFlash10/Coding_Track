754a448726c3cec31e732b942fe8d2cff55692b6
Satyam Shubham <57027429+ItsFlash10@users.noreply.github.com>
2021-12-09 22:38:16 +0530

    1306. Jump Game III

---
+++ December/Day9/code.java
@@ -0,0 +1,27 @@
+package Day9;
+
+//1306. Jump Game III
+//https://leetcode.com/problems/jump-game-iii/
+
+class jumgameIII {
+    public boolean canReach(int[] arr, int start) {
+      Queue<Integer> q = new LinkedList<>();
+      q.add(start);
+
+      while(!q.isEmpty())
+      {
+          int idx = q.poll();
+          if(arr[idx]==0)
+              return true;
+
+          int r = idx+arr[idx];
+          int l = idx-arr[idx];
+          arr[idx] = -1;     // make the visited index -1 
+          if(r<arr.length && arr[r]!=-1)
+              q.add(r);
+          if(l>=0 && arr[l]!=-1)
+              q.add(l);
+      }
+      return false;
+    }
+}
