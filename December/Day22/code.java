2d2121b394fc7a256ded940a0c0f75ae1ff0e9e0
Satyam Shubham <57027429+ItsFlash10@users.noreply.github.com>
2021-12-22 22:19:24 +0530

    143. Reorder List

---
+++ December/Day22/code.java
@@ -0,0 +1,40 @@
+package Day22;
+
+//143. Reorder List
+//https://leetcode.com/problems/reorder-list/
+
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public void reorderList(ListNode head) {
+            left = head;
+            dfs(head);
+        }
+
+        ListNode left = null;
+    public void dfs(ListNode right){
+        if(right == null)
+            return;
+
+        dfs(right.next);
+
+        if(left.next != null){
+            ListNode lNext = left.next;
+            left.next = right;
+            right.next = lNext;
+            left = lNext;
+        }
+        if(left.next == right)
+        {
+            left.next = null;
+        }        
+    }
+}
